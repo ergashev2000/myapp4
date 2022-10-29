a4e3efcef87554cba880cfc38e382e40ad2f45b8
islomjon <ergashevislomjon1020@gmail.com>
2022-10-29 19:19:51 +0500

    add

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("../../node_modules/express");
 const app = express();
- 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
+const server = require("http").Server(app);
+const io = module.exports.io = require('../../node_modules/socket.io/lib')(server)
+const path = require("path")
 
-
- 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+app.use(express.static(path.join(__dirname, '../../build')));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../build')));
+    console.log("DEBUG HERE", __dirname, path.join(__dirname+'../../build'));
+    //
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname+'../../build/index.html'));
+    })
+  }
+  //build mode
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../../public/index.html'));
+  })
