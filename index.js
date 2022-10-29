5ff99ef99257926ea76983e2ae9de2d56536edf6
islomjon <ergashevislomjon1020@gmail.com>
2022-10-29 18:57:13 +0500

    add

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+ 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
 });
-app.listen(port, () => {
-   console.log('Server is up!');
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
