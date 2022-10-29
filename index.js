4dd10ae8c43082ecba845e6194783849ea9279a5
islomjon <ergashevislomjon1020@gmail.com>
2022-10-29 19:05:12 +0500

    add

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  req.send('public/index.html')
   res
     .status(200)
     .send('Hello server is running')
